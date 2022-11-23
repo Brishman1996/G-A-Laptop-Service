95b728ff6ad1d360333c962dd5499fa0c5ef8726
Luz María <74826220@certus.edu.pe>
2022-11-22 23:36:52 -0500

    Revert "errores  conectados."
    This reverts commit e9a6f693dbbb07d476013e8f6f4c134b85266bd2.

--- src/main/java/pe/edu/certus/prueba/prueba/entity/UserData.java
+++ src/main/java/pe/edu/certus/prueba/prueba/entity/UserData.java
@@ -5,33 +5,4 @@ public class UserData {
 	private String nombre;
 	private String usuario;
 	private String contrasena;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getUsuario() {
-		return usuario;
-	}
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-	public String getContrasena() {
-		return contrasena;
-	}
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-	@Override
-	public String toString() {
-		return "UserData [id=" + id + ", nombre=" + nombre + ", usuario=" + usuario + ", contrasena=" + contrasena
-				+ "]";
-	}
 }
