e9a6f693dbbb07d476013e8f6f4c134b85266bd2
joaoriofrioflores <81723102+joaoriofrioflores@users.noreply.github.com>
2022-11-22 23:29:06 -0500

    errores  conectados.

--- src/main/java/pe/edu/certus/prueba/prueba/entity/RoomData.java
+++ src/main/java/pe/edu/certus/prueba/prueba/entity/RoomData.java
@@ -7,5 +7,36 @@ public class RoomData {
 	private long id;
 	private String name;
 	private String roomNumber;
+	
 	private String bedInfo;
+	public String getBedInfo() {
+		return bedInfo;
+	}
+	public void setBedInfo(String bedInfo) {
+		this.bedInfo = bedInfo;
+	}
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "RoomData [id=" + id + ", name=" + name + ", roomNumber=" + roomNumber + ", bedInfo=" + bedInfo + "]";
+	}
+	
 }
+
